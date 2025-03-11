243e7183bf43351940ef6a2dffdbc0a0874eedbc
Alex Fu <alexfu@fastmail.com>
2025-03-11 17:32:39 -0400

    add support for an uploaded filename

--- index.js
+++ index.js
@@ -3,14 +3,25 @@ const fs = require('fs');
 const FormData = require('form-data');
 const axios = require('axios');
 
+async function getFileReadStream() {
+  const file = core.getInput('file', { required: true });
+  const uploadedName = core.getInput('uploaded-filename');
+
+  if (uploadedName) {
+    await fs.promises.copyFile(file, uploadedName);
+    return fs.createReadStream(uploadedName);
+  }
+
+  return fs.createReadStream(file);
+}
+
 async function run() {
   try {
     const username = core.getInput('username', { required: true });
     const accessKey = core.getInput('access-key', { required: true });
-    const file = core.getInput('file', { required: true });
 
     const data = new FormData();
-    data.append('file', fs.createReadStream(file));
+    data.append('file', getFileReadStream());
 
     await axios.post('https://api-cloud.browserstack.com/app-live/upload', data, { auth: { username: username, password: accessKey } });
   } catch (error) {
